53ed52fee69a512c597249025aaad97cceee4319
snikliwp <wilk0146@algonquinlive.com>
2012-04-23 10:10:02 -0400

    fg

--- includes/theme_footer.php
+++ includes/theme_footer.php
@@ -10,6 +10,8 @@
  **/-->
 <footer>
 </footer>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script> <!--this is the JQuery library it should go at the bottom of the page--> 
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script> <!--this is the JQuery library it should go at the bottom of the page--> 
+	<script src="js/jquery.centredscroller.min.js"></script>
+	<script>$('.slideshow').centredScroller({autoAdvance:3000});</script>
 
 </html>
--- index.php
+++ index.php
@@ -13,9 +13,24 @@
 include "includes/theme_header.php"
 ?>
 
- <div id="main">
- <h3>Home Page - Main</h3>
- </div>
+<div id="main">
+<h3>Home Page - Main</h3>
+<div class="slideshow">
+	<div class="slides">
+		<div class="slide"><img src="slides/image1.JPG" alt="Photo showing depth of field" height="400" width="600" /></div>
+		<div class="slide"><img src="slides/image2.JPG" alt="Photo showing depth of field" height="400" width="600" /></div>
+		<div class="slide"><img src="slides/image3.JPG" alt="Photo showing depth of field" height="400" width="600" /></div>
+		<div class="slide"><img src="slides/image4.JPG" alt="Photo showing depth of field" /></div>
+		<div class="slide"><img src="slides/image5.JPG" alt="Photo showing depth of field" /></div>
+		<div class="slide"><img src="slides/image6.JPG" alt="Photo showing depth of field"  height="400" width="400"/></div>
+		<div class="slide"><img src="slides/image7.JPG" alt="Photo showing depth of field" /></div>
+		<div class="slide"><img src="slides/image8.JPG" alt="Photo showing depth of field" /></div> 
+
+
+
+
+		</div> <!-- end div class slides -->
+ </div> <!-- end div class slideshow -->
  
  <div id="right">
  <h3>Right</h3>
